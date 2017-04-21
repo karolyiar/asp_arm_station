54b7c26eab1536570c7a33da2c37f10d8f859454
karolyiar <karolyiar@gmail.com>
2017-04-21 20:25:27 +0200

    use Tools/nuget

--- GpioWrapper/CreateNugetPackage.bat
+++
@@ -1,8 +0,0 @@
-﻿REM automate nuget process
-
-mkdir lib\uap10.0
-copy bin\release\GpioWrapper.dll lib\uap10.0 /y
-del *.nupkg
-d:\download\nuget pack GpioWrapper.nuspec
-del lib\uap10.0\*.* /q
-rmdir lib /s /q
